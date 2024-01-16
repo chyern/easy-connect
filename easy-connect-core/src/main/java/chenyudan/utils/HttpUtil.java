d787bda4c47d8c723a8e906f2c8b52b3d0254f15
chenyu <yu.chen@narwal.com>
2024-01-16 18:21:55 +0800

    change

--- easy-connect-core/src/main/java/chenyudan/utils/HttpUtil.java
+++ easy-connect-core/src/main/java/chenyudan/utils/HttpUtil.java
@@ -1,8 +1,5 @@
 package chenyudan.utils;
 
-import com.chenyudan.parent.api.exception.BaseException;
-import com.chenyudan.parent.api.exception.enums.BaseExceptionEnum;
-import com.chenyudan.parent.core.constant.Constant;
 import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
@@ -12,11 +9,13 @@ import okhttp3.Request.Builder;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Description: TODO
@@ -29,11 +28,11 @@ public class HttpUtil {
 
     private static final OkHttpClient httpClient = new OkHttpClient().newBuilder().connectTimeout(10L, TimeUnit.SECONDS).callTimeout(30L, TimeUnit.SECONDS).build();
 
-    public static String get(String url, Map<String, Object> params) {
+    public static String get(String url, Map<String, Object> params) throws IOException {
         return get(url, params, null);
     }
 
-    public static String get(String url, Map<String, Object> params, Map<String, String> headers) {
+    public static String get(String url, Map<String, Object> params, Map<String, String> headers) throws IOException {
         String actualUrl = buildUrl(url, params);
         Request.Builder builder = new Request.Builder().url(actualUrl).method("GET", null);
         if (headers != null) {
@@ -42,11 +41,11 @@ public class HttpUtil {
         return getResult(builder);
     }
 
-    public static String postForJson(String url, Map<String, Object> params, Object body) {
+    public static String postForJson(String url, Map<String, Object> params, Object body) throws IOException {
         return postForJson(url, params, body, null);
     }
 
-    public static String postForJson(String url, Map<String, Object> params, Object body, Map<String, String> headers) {
+    public static String postForJson(String url, Map<String, Object> params, Object body, Map<String, String> headers) throws IOException {
         String actualUrl = buildUrl(url, params);
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=UTF-8"), new GsonBuilder().create().toJson(body));
         Request.Builder builder = new Request.Builder().url(actualUrl).method("POST", requestBody);
@@ -56,13 +55,14 @@ public class HttpUtil {
         return getResult(builder);
     }
 
-    public static String getResult(Builder builder) {
+    public static String getResult(Builder builder) throws IOException {
         try (Response response = httpClient.newCall(builder.build()).execute()) {
-            AssertUtil.isTrue(response.isSuccessful(), BaseExceptionEnum.CONNECT_ERROR);
+            if (!response.isSuccessful()) {
+                throw new IOException("response is not successful");
+            }
+
             ResponseBody responseBody = response.body();
             return responseBody != null ? responseBody.string() : null;
-        } catch (IOException e) {
-            throw new BaseException(BaseExceptionEnum.CONNECT_ERROR);
         }
     }
 
@@ -75,11 +75,13 @@ public class HttpUtil {
     public static String buildUrl(String url, Map<String, Object> params) {
         String actualUrl = url;
         if (params != null && !params.isEmpty()) {
-            List<String> paramList = LambdaUtil.mapToList(params, entry -> entry.getKey() + Constant.EQUAL_SIGN + entry.getValue());
-            String paramsStr = StringUtil.join(paramList, Constant.AMPERSAND);
-            actualUrl = actualUrl + Constant.QUESTION_MARK + paramsStr;
+
+            List<String> paramList = params.entrySet().stream()
+                    .filter(entry -> entry.getValue() != null)
+                    .map(entry -> entry.getKey() + "=" + entry.getValue()).collect(Collectors.toList());
+            String paramsStr = StringUtils.join(paramList, "&");
+            actualUrl = actualUrl + "?" + paramsStr;
         }
-        log.debug("http请求地址:{}", actualUrl);
         return actualUrl;
     }
 }
